99a2d5213141eb263a7e02c100fb335626d741d1
kitanaya <seb.heinzmann@gmail.com>
2025-04-14 17:54:59 +0200

    moved 'get_word_count' from main to stats, added import on main

--- main.py
+++ main.py
@@ -1,13 +1,8 @@
+from stats import get_word_count
+
 def get_book_text(filepath):
     with open(filepath, 'r', encoding="utf-8") as f:
         return f.read()
-
-
-def get_word_count(text):
-    words = text.split()
-    num_words = len(words)
-
-    return num_words
 
 
 def main():
---
+++ stats.py
@@ -0,0 +1,5 @@
+def get_word_count(text):
+    words = text.split()
+    num_words = len(words)
+
+    return num_words
