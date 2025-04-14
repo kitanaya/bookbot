70a4af0d1e41f1d7cf4ec02f24cb63b1bc08eea1
kitanaya <seb.heinzmann@gmail.com>
2025-04-14 22:19:12 +0200

    added sort and refined output together with boots :)

--- main.py
+++ main.py
@@ -1,17 +1,41 @@
 from stats import get_word_count
 from stats import get_char_count
+from stats import get_key_value
+from pathlib import Path
 
 def get_book_text(filepath):
+    path = Path(filepath)
+    if not path.exists():
+        print(f"Error: File {filepath} does not exist!")
+        return ""
     with open(filepath, 'r', encoding="utf-8") as f:
         return f.read()
+
 
 
 def main():
     book = f"books/frankenstein.txt"
     text = get_book_text(book)
     num_words = get_word_count(text)
-    print(f"{num_words} words found in the document")
     num_chars = get_char_count(text)
-    print(f"{num_chars}")
+    sorted_list = get_key_value(num_chars)
+    
+    header = f"""
+============ BOOKBOT ============
+Analyzing book found at {book}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------
+"""
+    footer = f"============= END ==============="
+
+    print(header)
+    for letter in sorted_list:
+        if letter["char"].isalpha():
+            # Pad each count number to be consistently aligned
+            # print(f'{letter["char"]}: {str(letter["count"]).rjust(6)}')
+            print(f'{letter["char"]}: {letter["count"]}')
+    print(footer)
+
 
 main()
--- stats.py
+++ stats.py
@@ -3,6 +3,7 @@ def get_word_count(text):
     num_words = len(words)
     return num_words
 
+
 def get_char_count(text):
     num_chars = {}
     for char in text.lower():
@@ -11,3 +12,15 @@ def get_char_count(text):
         else:
             num_chars[char] += 1
     return num_chars
+
+
+# def sort_on(dict):
+#     return dict["num"]
+
+def get_key_value(chars):
+    sorted_list = []
+    for char in chars:
+        new_dict = {"char": char, "count": chars[char]}
+        sorted_list.append(new_dict)
+    sorted_list.sort(reverse=True, key=lambda x: x["count"])
+    return sorted_list
